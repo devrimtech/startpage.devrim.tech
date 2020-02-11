760be35c347698f5f882cac46d79e23ff3a9ebfd
KaiDevrim <kkgitkraken@devrim.tech>
2020-02-10 16:54:58 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,55 +19,55 @@ const formElement = document.getElementById('search-form');
  * Return a string containing the formatted current date and time.
  */
 function getDateTime() {
-    // const dateTime = new Date();
-    // let day = dateTime.getDate();
-    // let month = dateTime.getMonth() + 1;
-    // let hour = dateTime.getHours();
-    // let minutes = dateTime.getMinutes();
-    // let seconds = dateTime.getSeconds();
+    const dateTime = new Date();
+    let day = dateTime.getDate();
+    let month = dateTime.getMonth() + 1;
+    let hour = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
 
-    // let hourPST = hour - 7;
-    // hour = hourPST;
+    let hourPST = hour - 7;
+    hour = hourPST;
 
-    // if (hour < 0) {
-    //     hour = 24 + hour;
-    // }
+    if (hour < 0) {
+        hour = 24 + hour;
+    }
 
-    // let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
-    // let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
+    let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 
-    // return date + '\n' + time;
+    return date + '\n' + time;
 
-    var currentTime = new Date();
+    // var currentTime = new Date();
 
-    var currentHours = currentTime.getHours() + 4;
-    var currentMinutes = currentTime.getMinutes();
-    var currentSeconds = currentTime.getSeconds();
-    var currentDay = currentTime.getDate() - 1;
-    var currentMonth = currentTime.getMonth();
-    var currentYear = currentTime.getFullYear();
+    // var currentHours = currentTime.getHours() + 4;
+    // var currentMinutes = currentTime.getMinutes();
+    // var currentSeconds = currentTime.getSeconds();
+    // var currentDay = currentTime.getDate() - 1;
+    // var currentMonth = currentTime.getMonth();
+    // var currentYear = currentTime.getFullYear();
 
 
-    // Pad the minutes and seconds with leading zeros, if required
-    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+    // // Pad the minutes and seconds with leading zeros, if required
+    // currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    // currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
-    // Choose either "AM" or "PM" as appropriate
-    var timeOfDay = (currentHours < 12) ? "PM" : "AM";
+    // // Choose either "AM" or "PM" as appropriate
+    // var timeOfDay = (currentHours < 12) ? "PM" : "AM";
 
-    // Convert the hours component to 12-hour format if needed
-    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    // // Convert the hours component to 12-hour format if needed
+    // currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
 
-    // Convert an hours component of "0" to "12"
-    currentHours = (currentHours == 0) ? 12 : currentHours;
+    // // Convert an hours component of "0" to "12"
+    // currentHours = (currentHours == 0) ? 12 : currentHours;
 
-    // Compose the string for display
-    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+    // // Compose the string for display
+    // var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
-    var myTime = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay + '\n' + currentMonth + "/" + currentDay + "/" + currentYear;
+    // var myTime = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay + '\n' + currentMonth + "/" + currentDay + "/" + currentYear;
 
-    // Update the time display
-    //document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+    //Update the time display
+    document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 
     return myTime;
 }
